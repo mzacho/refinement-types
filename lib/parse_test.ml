1d9ecec4f7fe98e0abd5fed17227ec39b1c508cf
Wolfgang Meier <womeier@posteo.de>
2023-11-14 16:55:37 +0100

    test + cleanup parser config

--- lib/parse_test.ml
+++ lib/parse_test.ml
@@ -69,3 +69,36 @@ let%test "nat type predicate" =
 
 let%test "negation" =
   string_to_type "int{x: ~ False}" = T_Refined (B_Int, x', P_Neg P_False)
+
+let%test "int refined to: 0 = x" =
+  string_to_type "int{x: x = 0}"
+  = T_Refined (B_Int, x', P_Op (O_Eq, P_Var x', P_Int 0))
+
+let%test "int refined to: 0 != x" =
+  string_to_type "int{x: x != 0}"
+  = T_Refined (B_Int, x', P_Neg (P_Op (O_Eq, P_Var x', P_Int 0)))
+
+let%test "int refined to: 1 <= x < 4" =
+  string_to_type "int{x: (1 <= x) & (x < 4)}"
+  = T_Refined
+      ( B_Int,
+        x',
+        P_Conj (P_Op (O_Le, P_Int 1, P_Var x'), P_Op (O_Lt, P_Var x', P_Int 4))
+      )
+
+let%test "int refined to: 10 > (x - 1) & ((x + 1) >= 4" =
+  string_to_type "int{x: (10 > (x - 1)) & ((x + 1) >= 4)}"
+  = T_Refined
+      ( B_Int,
+        x',
+        P_Conj
+          ( P_Op (O_Gt, P_Int 10, P_Op (O_Sub, P_Var x', P_Int 1)),
+            P_Op (O_Ge, P_Op (O_Add, P_Var x', P_Int 1), P_Int 4) ) )
+
+let%test "precedence of >=, =, &" =
+  string_to_type "int{x: x >= 3 & x = 3}"
+  = T_Refined
+      ( B_Int,
+        x',
+        P_Conj (P_Op (O_Ge, P_Var x', P_Int 3), P_Op (O_Eq, P_Var x', P_Int 3))
+      )
