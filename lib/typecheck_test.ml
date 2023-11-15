a1e020e2b10744c7b2abceea3630a56f422c5598
Martin Zacho <hi@martinzacho.net>
2023-11-15 11:45:14 +0100

    implement typecheck.synth (wip) and typecheck.sub

--- lib/typecheck_test.ml
+++ lib/typecheck_test.ml
@@ -11,8 +11,46 @@ let%test "Fail when trying to check an abstraction against an int" =
   try Solver.check (Typecheck.check g e t)
   with Typecheck.Invalid_arrow_type _ -> true
 
-let%test "Integer constant 0 checks" =
+let%test "Constant 0 checks" =
   let e = Parse.string_to_program "0" in
   let g = Typecheck.E_Empty in
   let t = Parse.string_to_type "int{v: False}" in
+  not (Solver.check (Typecheck.check g e t))
+
+let%test "Constant 0 refined precicely" =
+  let e = Parse.string_to_program "0" in
+  let g = Typecheck.E_Empty in
+  let t = Parse.string_to_type "int{v: v = 0}" in
+  Solver.check (Typecheck.check g e t)
+
+let%test "Constant 5 refined gt 4" =
+  let e = Parse.string_to_program "5" in
+  let g = Typecheck.E_Empty in
+  let t = Parse.string_to_type "int{v: v > 4}" in
+  Solver.check (Typecheck.check g e t)
+
+let%test "Constant 5 refined lt 10" =
+  let e = Parse.string_to_program "5" in
+  let g = Typecheck.E_Empty in
+  let t = Parse.string_to_type "int{v: v < 10}" in
+  Solver.check (Typecheck.check g e t)
+
+let%test "Var checks with same type as in environment" =
+  let e = Parse.string_to_program "y" in
+  let t = Parse.string_to_type "int{v: True}" in
+  let g = Typecheck.E_Cons ("y", t, Typecheck.E_Empty) in
+  Solver.check (Typecheck.check g e t)
+
+let%test "Var checks with subtype of type in environment" =
+  let e = Parse.string_to_program "x" in
+  let s = Parse.string_to_type "int{v: v = 42}" in
+  let g = Typecheck.E_Cons ("x", s, Typecheck.E_Empty) in
+  let t = Parse.string_to_type "int{v: True}" in
+  Solver.check (Typecheck.check g e t)
+
+let%test "Var doesn't check with supertype of type in environment" =
+  let e = Parse.string_to_program "x" in
+  let s = Parse.string_to_type "int{v: True}" in
+  let g = Typecheck.E_Cons ("x", s, Typecheck.E_Empty) in
+  let t = Parse.string_to_type "int{v: v = 42}" in
   not (Solver.check (Typecheck.check g e t))
