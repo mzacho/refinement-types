c1935a62425cf7cb647e4ad8e3882ded966f9cde
Martin Zacho <hi@martinzacho.net>
2023-12-05 16:19:34 +0100

    Add P_FunApp branch in check_sort

--- lib/typecheck.ml
+++ lib/typecheck.ml
@@ -241,7 +241,9 @@ let rec check_sort (g : logic_env) (p : L.pred) (s : L.sort) : bool =
             let _, s' = List.find (fun (y, _) -> String.equal y x) g in
             s' = L.S_Int
           with Not_found -> true)
-      | _ -> false)
+      | P_FunApp (_, _) -> true (* TODO: lookup codomain of uninterpreted fun *)
+      | _ -> false
+  )
   | L.S_Bool -> failwith "unimplemented"
   | L.S_TyCtor _ -> failwith "unimplemented"
 
