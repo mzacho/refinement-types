6c61a8628efead6f1d15589745bed95d41c2aede
Wolfgang Meier <womeier@posteo.de>
2023-12-07 21:53:11 +0100

    basic cli

--- bin/compiler.ml
+++
@@ -1,8 +0,0 @@
-module M = Atlp2023
-
-let _ =
-  let e = M.Parse.string_to_expr "x" in
-  let ty = M.Parse.string_to_type "int{x: False}" in
-  let gamma = M.Typecheck.E_Empty in
-  let cs = M.Typecheck.check gamma e ty in
-  M.Solver.check cs
